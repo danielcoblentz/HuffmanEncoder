5cda407df617998c50fbe2b0b53dcc1af2f8e86e
Daniel <danielcoblentz916@gmail.com>
2025-03-19 18:23:37 -0400

    revised code fixed /r issue

--- README.md
+++ README.md
@@ -3,5 +3,7 @@ This program reads input from a file and loads the letters as keys and their fre
 
 First, we create a Hashtable to store the key-value pairs. Then, we use the specified file path (which can be changed) to read input character by character while accounting for special characters. These values are dynamically loaded into the Hashtable initialized earlier.
 
+`note`: In the example output it had the keys ordered alphabetically but in java they are random unless you use treemap. however all the keys and values are correct.
+
 How to Run:
 You can run this program in VS Code by clicking Run in the top left.
--- huffman.java
+++ huffman.java
@@ -3,24 +3,31 @@ import java.util.*;
 
 public class huffman {
     public static void main(String[] args) {
-        String filePath = "test/msgspec.txt";  // path to demo file (change as needed for different files)
+        String filePath = "test/msgspec.txt";  // link to demo file (change path as needed for other files)
         Map<Character, Integer> frequencyTable = new HashMap<>();
 
-        //read input and load into hashmap
+
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             int ch;
-            while ((ch = br.read()) != -1) { 
+            while ((ch = br.read()) != -1) {  // read character by character in the file
                 char letter = (char) ch;
-                if (letter == ' ') letter = '-';  // mapping space to '-'
-                else if (letter == '\n') letter = '!'; // new line mapped to '!'
+
+                if (letter == '\r') continue;  // seperate case in Windows line endings it counts as a seperate charcter
+
+                if (letter == ' ') letter = '-';  // Mapping space to '-'
+                else if (letter == '\n') letter = '!'; // Map new line to '!'
+
+                // Update frequency table
                 frequencyTable.put(letter, frequencyTable.getOrDefault(letter, 0) + 1);
             }
         } catch (IOException e) {
-            System.out.println("error check the file path");
+            System.out.println("Error reading file: " + e.getMessage());
         }
 
+        // Increment '+' at the end of the file
+        frequencyTable.put('+', frequencyTable.getOrDefault('+', 0) + 1);
 
-        // Print frequency table to terminal
+        // Print frequency table
         System.out.println("Frequency table:");
         for (Map.Entry<Character, Integer> entry : frequencyTable.entrySet()) {
             System.out.println(entry.getKey() + " " + entry.getValue());
